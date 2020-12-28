d3b2e0d388c3dbf34010dce20b75ff37a30e7a7a
WatDuhHekBro <44940783+WatDuhHekBro@users.noreply.github.com>
2020-12-27 19:43:45 -0600

    Added some configuration

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 use actix::Actor;
 use actix_files::Files;
 use actix_web::{App, HttpServer};
+use serde_json::from_str;
+use std::fs;
 use std::sync::{Arc, Mutex};
-use ztransfer::{structures::Broadcast, transfer, websocket};
+use ztransfer::{
+    structures::{Broadcast, ServerSettings},
+    transfer, websocket,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let settings;
+
+    // Read settings.json
+    if let Ok(settings_file) = fs::read_to_string("settings.json") {
+        settings = from_str::<ServerSettings>(&settings_file)?;
+    } else {
+        settings = ServerSettings {
+            host: String::from("127.0.0.1"),
+            port: 8080,
+        };
+    }
+
+    // Then initialize the server
     let broadcaster = Arc::new(Mutex::new(Broadcast {}.start()));
-    std::fs::create_dir_all("./tmp").unwrap();
+
+    // Make sure that the tmp directory exists
+    fs::create_dir_all("./tmp").unwrap();
+
     HttpServer::new(move || {
         // The order in which you add services/routes DOES matter.
         // Precedence is first come first serve, so the most general (static serve) should come last.
@@ -22,7 +43,7 @@ async fn main() -> std::io::Result<()> {
             // Host static files from "public" and host "index.html" as the index.
             .service(Files::new("/", "public").index_file("index.html"))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(format!("{}:{}", settings.host, settings.port))?
     .run()
     .await
 }
